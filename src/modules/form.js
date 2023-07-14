364c6d2abff3a19846512679380fe98b2b6abf04
Iana Shtina <sianalucky@gmail.com>
2023-07-14 19:06:41 +0300

    final

--- src/modules/form.js
+++ src/modules/form.js
@@ -1,76 +1,43 @@
 export default {
   init() {
-    const form = document.querySelector('.form__form');
+    const forms = document.querySelectorAll('.toSend');
 
-    form.addEventListener('submit', formSended);
+    forms.forEach((f) => {
+      f.addEventListener('submit', formSended);
 
-    async function formSended(e) {
-      e.preventDefault();
-      console.log('formSended', e);
-
-      let formData = new FormData(form);
-      console.log('formData', formData);
-
-      let response = await fetch('mail.php', {
-        method: 'POST',
-        body:formData,
-
-        
-      });
-
-      console.log('response', response);
-      if(response.ok) {
-        let result = await response.json;
-       
-        console.log('result', result, response);
-        form.reset();
-        form.classList.add('sended');
-
-        setTimeout(() => {
-          form.classList.remove('sended');;
-        }, 3000);
-
-      } else {
-        console.log('ошибка');
+      async function formSended(e) {
+        e.preventDefault();
+        console.log('formSended', e);
+  
+        let formData = new FormData(f);
+        console.log('formData', formData);
+  
+        let response = await fetch('mail.php', {
+          method: 'POST',
+          body:formData,
+  
+          
+        });
+  
+        console.log('response', response);
+        if(response.ok) {
+          let result = await response.json;
+         
+          console.log('result', result, response);
+          f.reset();
+          f.classList.add('sended');
+  
+          setTimeout(() => {
+            f.classList.remove('sended');;
+          }, 3000);
+  
+        } else {
+          console.log('ошибка');
+        }
+  
+  
       }
 
-
-    }
-
-
-    const formPopup = document.querySelector('.popup__form');
-
-    formPopup.addEventListener('submit', formSended);
-
-    async function formSended(e) {
-      e.preventDefault();
-      console.log('formSended', e);
-
-      let formData = new FormData(formPopup);
-      console.log('formData', formData);
-
-      let response = await fetch('mail.php', {
-        method: 'POST',
-        body:formData,
-      });
-
-      console.log('response', response);
-      if(response.ok) {
-        let result = await response.json;
-       
-        console.log('result', result, response);
-        formPopup.reset();
-        formPopup.classList.add('sended');
-
-        setTimeout(() => {
-          formPopup.classList.remove('sended');;
-        }, 3000);
-
-      } else {
-        console.log('ошибка');
-      }
-
-
-    }
+    })
   }
 }
