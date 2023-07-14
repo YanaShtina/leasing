69beb3412dd10e13bbba3e4218c1d56d2af29ff8
Iana Shtina <sianalucky@gmail.com>
2023-07-14 18:55:01 +0300

    final

--- src/modules/form.js
+++ src/modules/form.js
@@ -11,7 +11,7 @@ export default {
       let formData = new FormData(form);
       console.log('formData', formData);
 
-      let response = await fetch('../mail.php', {
+      let response = await fetch('mail.php', {
         method: 'POST',
         body:formData,
 
@@ -19,11 +19,53 @@ export default {
       });
 
       console.log('response', response);
-
       if(response.ok) {
         let result = await response.json;
-        console.log('result', result, response)
+       
+        console.log('result', result, response);
         form.reset();
+        form.classList.add('sended');
+
+        setTimeout(() => {
+          form.classList.remove('sended');;
+        }, 3000);
+
+      } else {
+        console.log('ошибка');
+      }
+
+
+    }
+
+
+    const formPopup = document.querySelector('.popup__form');
+
+    formPopup.addEventListener('submit', formSended);
+
+    async function formSended(e) {
+      e.preventDefault();
+      console.log('formSended', e);
+
+      let formData = new FormData(formPopup);
+      console.log('formData', formData);
+
+      let response = await fetch('mail.php', {
+        method: 'POST',
+        body:formData,
+      });
+
+      console.log('response', response);
+      if(response.ok) {
+        let result = await response.json;
+       
+        console.log('result', result, response);
+        formPopup.reset();
+        formPopup.classList.add('sended');
+
+        setTimeout(() => {
+          formPopup.classList.remove('sended');;
+        }, 3000);
+
       } else {
         console.log('ошибка');
       }
